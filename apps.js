1407339e43a8511b22f0c29ff60ebd7b09a60e50
sophia lenharo <sophi@uept48-13669-e.aluno.pr.senac.br>
2023-10-31 13:27:06 -0300

    adicionando um console log

--- apps.js
+++ apps.js
@@ -1,1 +1,6 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
+console.log ("aplicação está sendo conectado ao banco de dados")
+
+//mais um console log teste:
+console.log ("o Paulo fez esse commit sem você na aula!")
+console.log ("enviando para o GitHub")
